0f4ae04d507612ef5618390251faa113bb816551
Michael Totaro <mtotaro@ncsu.edu>
2022-06-23 14:47:35 -0400

    Adding network_inventory.py draft

---
+++ network_inventory.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+A basic network inventory generation script.
+
+Goal:
+- Create a csv inventory file device name, software version, uptime,
+  serial number.
+"""
+
+# Script entry point
+if __name__ == "__main__":
+    print("Creating a network inventory script.")
+
+    # Load pyATS testbed into script
+
+    # Connect to network devices.
+
+    # Run commands to gather output from devices
+
+    # Build inventory report data structure
+
+    # Generate CSV file of data
